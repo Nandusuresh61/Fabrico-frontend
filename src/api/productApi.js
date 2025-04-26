c91a7c8ddc2ffad1c4524a779074708254285341
Nandu Suresh <nandusuresh61@gmail.com>
2025-04-27 02:30:18 +0530

    majot filter issue sorted

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -10,7 +10,7 @@ export const updateProductStockApi = (productId, variantId, quantity) =>
   API.put(`/products/${productId}/variants/${variantId}/update-stock`, { quantity });
 
 export const getAllProductsForUsersApi = (params) => 
-  API.get('/products', { 
+  API.get('/products/users', { 
     params: {
       ...params,
       minPrice: Number(params.minPrice),
